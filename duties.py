8862bdb6a95185f582bc8b4b7448daabe0c014d3
Timothée Mazzucotelli <dev@pawamoy.fr>
2025-01-23 13:17:02 +0100

    style: Format

--- duties.py
+++ duties.py
@@ -58,7 +58,7 @@ def changelog(ctx: Context, bump: str = "") -> None:
 
 
 @duty(pre=["check_quality", "check_types", "check_docs", "check_dependencies", "check-api"])
-def check(ctx: Context) -> None:  # noqa: ARG001
+def check(ctx: Context) -> None:
     """Check it all!"""
 
 
