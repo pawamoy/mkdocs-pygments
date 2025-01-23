ca6195816798de40967c984625138fe1454a62cf
Timothée Mazzucotelli <dev@pawamoy.fr>
2025-01-23 13:17:32 +0100

    refactor: Inform about public project being a no-op
    Issue-1: https://github.com/pawamoy/mkdocs-pygments/issues/1

--- src/mkdocs_pygments/plugin.py
+++ src/mkdocs_pygments/plugin.py
@@ -2,9 +2,17 @@
 
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from mkdocs.config.base import Config as BaseConfig
 from mkdocs.config.config_options import Type as MkType
-from mkdocs.plugins import BasePlugin
+from mkdocs.plugins import BasePlugin, get_plugin_logger
+
+if TYPE_CHECKING:
+    from mkdocs.config.defaults import MkDocsConfig
+
+
+logger = get_plugin_logger("mkdocs_pygments")
 
 
 class PygmentsConfig(BaseConfig):
@@ -18,3 +26,7 @@ class PygmentsConfig(BaseConfig):
 
 class PygmentsPlugin(BasePlugin[PygmentsConfig]):
     """The MkDocs plugin to inject Pygments style sheets."""
+
+    def on_config(self, config: MkDocsConfig) -> None:  # noqa: D102
+        if "pygments" in config.plugins:
+            logger.info("The public version of the mkdocs-pygments plugin is a no-op.")
